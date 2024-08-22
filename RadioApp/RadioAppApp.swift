32bdc28c9a5ca11f4639d1e6a7afa21c18082f10
kmazure <kmazure@teamdoc.fr>
2024-08-22 20:57:04 +0200

    Initial Commit

---
+++ RadioApp/RadioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadioAppApp.swift
+//  RadioApp
+//
+//  Created by teamdoc on 22/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct RadioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
