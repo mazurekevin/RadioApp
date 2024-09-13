e0a0fd7da3bc368fee6bc61d123cfd64502ca68b
mazurekevin <kevin.mazure.km@gmail.com>
2024-09-13 20:59:56 +0200

    DataManager

---
+++ RadioApp/Models/DataManager.swift
@@ -0,0 +1,34 @@
+//
+//  DataManager.swift
+//  RadioApp
+//
+//  Created by teamdoc on 13/09/2024.
+//
+
+import Foundation
+
+class DataManager: NSObject {
+
+    static var instance = DataManager()
+
+    /// radio
+    var radioList: [RadioStation]
+    var favoriteRadioList: [RadioStation]
+    
+    /// podcast
+    var podcastList: [Podcast]
+    var favoritePodcastList: [Podcast]
+    
+    override init() {
+        radioList = []
+        favoriteRadioList = []
+        podcastList = []
+        favoritePodcastList = []
+        super.init()
+    }
+
+    func reset() {
+        DataManager.instance = DataManager()
+    }
+    
+}
